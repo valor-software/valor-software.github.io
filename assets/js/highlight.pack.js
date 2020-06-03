9bf7508b8f0b453105e2f63b1088a213b6949fe4
Iliyavostrikov <iliya.vostrikov@valor-software.com>
2020-06-03 11:20:13 +0300

    Updates

--- assets/js/highlight.pack.js
+++ assets/js/highlight.pack.js
@@ -2,6 +2,6 @@
 <head><title>404 Not Found</title></head>
 <body>
 <center><h1>404 Not Found</h1></center>
-<hr><center>nginx/1.16.1 (Ubuntu)</center>
+<hr><center>nginx/1.17.10 (Ubuntu)</center>
 </body>
 </html>
