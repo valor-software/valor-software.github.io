ff3d4d62342f316dd3bebbe4afe8b26ba31395f1
MuravlovaSvetlana <ushakovasvetlana94kh@gmail.com>
2021-11-22 17:12:00 +0200

    added techoloigies list

--- libs/route-pages/home-page/src/lib/home-page.component.ts
+++ libs/route-pages/home-page/src/lib/home-page.component.ts
@@ -35,6 +35,89 @@ const slideModel = [
     }
 ];
 
+const technologiesList = [
+    {
+        id: 'angular',
+        img: 'assets/img/technologies/angular.png',
+        active: false
+    },
+    {
+        id: 'react',
+        img: 'assets/img/technologies/react.png',
+        active: false
+    },
+    {
+        id: 'react_native',
+        img: 'assets/img/technologies/react_native.png',
+        active: false
+    },
+    {
+        id: 'ruby',
+        img: 'assets/img/technologies/ruby.png',
+        active: false
+    },
+    {
+        id: 'vue',
+        img: 'assets/img/technologies/vue.png',
+        active: false
+    },
+    {
+        id: 'nest_js',
+        img: 'assets/img/technologies/nest_js.png',
+        active: false
+    },
+    {
+        id: 'python',
+        img: 'assets/img/technologies/python.png',
+        active: false
+    },
+    {
+        id: 'go',
+        img: 'assets/img/technologies/go.png',
+        active: false
+    },
+    {
+        id: 'ionic',
+        img: 'assets/img/technologies/ionic.png',
+        active: false
+    },
+    {
+        id: 'native_script',
+        img: 'assets/img/technologies/native_script.png',
+        active: false
+    },
+    {
+        id: 'google',
+        img: 'assets/img/technologies/google_cloud.png',
+        active: false
+    },
+    {
+        id: 'firebase',
+        img: 'assets/img/technologies/firebase.png',
+        active: false
+    },
+    {
+        id: 'heroku',
+        img: 'assets/img/technologies/heroku.png',
+        active: false
+    },
+    {
+        id: 'aws',
+        img: 'assets/img/technologies/aws.png',
+        active: false
+    },
+    {
+        id: 'lang',
+        img: 'assets/img/technologies/lang.png',
+        active: false
+    },
+    {
+        id: 'js',
+        img: 'assets/img/technologies/java_script.png',
+        active: false
+    }
+];
+
 
 @Component({
     // eslint-disable-next-line @angular-eslint/component-selector
@@ -64,8 +147,21 @@ export class HomePageComponent {
             },
         }
     };
-
+    technologiesList: typeof technologiesList = technologiesList;
 
     constructor() {
+    }
+
+    checkIndex(index: number): boolean {
+        index++;
+        return !!(index && !(index % 4));
+    }
+
+    sortProjects(technology: {id: string, active: boolean, img: string}): void {
+        this.technologiesList.map( item => {
+            item.active = false;
+        });
+
+        technology.active = true;
     }
 }
