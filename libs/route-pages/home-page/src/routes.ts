f3baaaea76585c2439bf898a3a4452220eb4ff8f
MuravlovaSvetlana <ushakovasvetlana94kh@gmail.com>
2021-11-30 11:01:08 +0200

    test component

--- libs/route-pages/home-page/src/routes.ts
+++ libs/route-pages/home-page/src/routes.ts
@@ -1,4 +1,5 @@
 import { HomePageComponent } from "./home-page.module";
+import {TestComponent} from "./test.component";
 
-export const routes = [{ path: '', component: HomePageComponent }];
+export const routes = [{ path: '', component: TestComponent }];
 
---
+++ libs/route-pages/home-page/src/test.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    // eslint-disable-next-line @angular-eslint/component-selector
+    selector: 'test',
+    templateUrl: './test.component.html'
+})
+export class TestComponent {}
