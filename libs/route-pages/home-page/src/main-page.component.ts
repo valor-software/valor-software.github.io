f094863d0e7fe9a9351eac0b18d20769e0ca6c7c
SvetlanaMuravlova <svetlana.muravlova@valor-software.com>
2022-04-01 15:52:15 +0300

    added opportunity for text editor

--- libs/route-pages/home-page/src/main-page.component.ts
+++ libs/route-pages/home-page/src/main-page.component.ts
@@ -347,4 +347,9 @@ export class MainPageComponent {
 
         return window.innerHeight > 800 && window.devicePixelRatio*100 === 100;
     }
+
+    checkCursor(event: MouseEvent) {
+
+        console.log(event);
+    }
 }
