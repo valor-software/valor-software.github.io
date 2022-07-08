43e2cb6da3ba7b7d6df5d3e19be19f64a9d10ec4
newmacbook <1234>
2022-07-08 14:32:26 +0400

    feat(ashes-page): made library buildable 1

--- libs/common-docs/src/index.ts
+++ libs/common-docs/src/index.ts
@@ -1,3 +1,4 @@
 export * from './common-docs.module';
 export * from './components/technologies/types';
 export * from './components/technologies/technologies.component';
+export * from './components/comparison-slider/comparison-slider.component';
