9716f49b754865c7b2783a0bd55c198e98874e12
Andson Santos <andson.josef@gmail.com>
2022-09-14 16:21:03 -0300

    remove articles from scully deploy

--- scully/plugins/blog-route-plugin.ts
+++ scully/plugins/blog-route-plugin.ts
@@ -1,21 +1,21 @@
-import { HandledRoute, registerPlugin } from "@scullyio/scully";
-import { articlesRefactoringTitlesList } from "../../assets/articles/articlesList";
-import { linksFromOldSite } from "../../assets/articles/brokenRoutes";
+// import { HandledRoute, registerPlugin } from "@scullyio/scully";
+// import { articlesRefactoringTitlesList } from "../../assets/articles/articlesList";
+// import { linksFromOldSite } from "../../assets/articles/brokenRoutes";
 
-function blogRoutePlugin(route: string, config = {}): Promise<HandledRoute[]> {
-    const oldLinks = Object.keys(linksFromOldSite);
-    const routes = [];
-    articlesRefactoringTitlesList.map((item: string) => {
-        routes.push({route: `/articles/${item}`});
-        // routes.push({route: `/articles/${item}.html`});
-    });
+// function blogRoutePlugin(route: string, config = {}): Promise<HandledRoute[]> {
+//     const oldLinks = Object.keys(linksFromOldSite);
+//     const routes = [];
+//     articlesRefactoringTitlesList.map((item: string) => {
+//         routes.push({route: `/articles/${item}`});
+//         routes.push({route: `/articles/${item}.html`});
+//     });
 
-    oldLinks.map(item => {
-        routes.push({route: `/articles/${item}`});
-    })
+//     oldLinks.map(item => {
+//         routes.push({route: `/articles/${item}`});
+//     })
 
-    return Promise.resolve(routes);
-}
+//     return Promise.resolve(routes);
+// }
 
-const validator = async (conf) => [];
-registerPlugin('router', 'blogRoute', blogRoutePlugin, validator);
+// const validator = async (conf) => [];
+// registerPlugin('router', 'blogRoute', blogRoutePlugin, validator);
