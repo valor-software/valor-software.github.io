98868594f4c4b6bfeb5bea7913e8989baf068050
Andson Santos <andson.josef@gmail.com>
2022-09-15 19:49:23 -0300

    feat(services): Service DevOps Page

--- libs/route-pages/services-page/src/routes.ts
+++ libs/route-pages/services-page/src/routes.ts
@@ -1,4 +1,8 @@
+import { DevOpsComponent } from './pages/devops/devops.component';
 import {ServicesPageComponent} from "./services-page.component";
 
-export const routes = [{ path: '', component: ServicesPageComponent }];
+export const routes = [
+  { path: '', component: ServicesPageComponent },
+  { path: 'devops', component: DevOpsComponent }
+];
 
