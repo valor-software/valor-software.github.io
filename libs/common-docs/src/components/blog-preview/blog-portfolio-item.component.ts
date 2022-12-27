efce9222f9b8329dbfa5ede7d8b97a08c2d3a5b7
Andson Josef <andson.josef@gmail.com>
2022-12-27 11:39:04 -0300

    fix(blog): fix preview img cropped

--- libs/common-docs/src/components/blog-preview/blog-portfolio-item.component.ts
+++ libs/common-docs/src/components/blog-preview/blog-portfolio-item.component.ts
@@ -9,7 +9,7 @@ import { IPortfolio } from "../../models/portfolio.interface";
     template:`
         <div class="article-item">
             <div class="img-container zoom-img-scale">
-                <img class="w-full h-full object-cover" [src]="article ? article.bgImg : project?.previewImg" [alt]="article ? article.title : project?.name + 'img'">
+                <img class="w-full h-full" [src]="article ? article.bgImg : project?.previewImg" [alt]="article ? article.title : project?.name + 'img'">
             </div>
             <div class="text-container">
                 <p>{{article ? article.title : project?.name}}</p>
