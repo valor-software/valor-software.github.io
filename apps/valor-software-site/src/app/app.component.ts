bc8d2fc0a0e2348865499fca6846516fc181fb5c
danyaZh <reus11fan@gmail.com>
2023-03-23 11:43:44 +0200

    feat: added cookie consent banner

--- apps/valor-software-site/src/app/app.component.ts
+++ apps/valor-software-site/src/app/app.component.ts
@@ -1,101 +1,122 @@
-import { Component, Inject, HostListener, OnDestroy, AfterViewInit } from '@angular/core';
-import { SeoService } from "@valor-software/common-docs";
+import { Component, Inject, HostListener, OnDestroy, AfterViewInit, OnInit } from '@angular/core';
+import { SeoService } from '@valor-software/common-docs';
 import { NavigationEnd, Router, RoutesRecognized } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
-import { filter, pairwise } from "rxjs/operators";
-import { Subscription } from "rxjs";
+import { filter, pairwise } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { NgcCookieConsentService } from 'ngx-cookieconsent';
+import { CookieService } from 'ngx-cookie-service';
 
 const notFoundPageUrl = '/404';
 declare const gtag: any;
 
 @Component({
-  selector: 'valor-software-site-base-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'valor-software-site-base-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnDestroy, AfterViewInit {
-    title = 'valor-software-site';
-    scrollPosition = 0;
-    $routeEvents?: Subscription;
-    $gaRouteEvents?: Subscription;
-    $routerEventNavigationEnd: Subscription;
-    showFooterAndHeader = true;
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
+	private statusChangeSubscription!: Subscription;
 
-    @HostListener('window:beforeunload') private onReload() {
-        window.sessionStorage.setItem('scrollPosition', window.scrollY.toString());
-    }
+	title = 'valor-software-site';
+	scrollPosition = 0;
+	$routeEvents?: Subscription;
+	$gaRouteEvents?: Subscription;
+	$routerEventNavigationEnd: Subscription;
+	showFooterAndHeader = true;
 
-    constructor(
-      private seo: SeoService,
-      private router: Router,
-      @Inject(DOCUMENT) private document: any
-    ) {
-        const scrollItem = window.sessionStorage.getItem('scrollPosition');
-        this.scrollPosition = scrollItem || scrollItem == '0' ? Number(scrollItem) : 0;
-        this.$routerEventNavigationEnd = router.events.pipe(
-            filter(evt => evt instanceof NavigationEnd)
-        ).subscribe(res => {
-            this.showFooterAndHeader = !!(this.router.url === notFoundPageUrl);
-        });
-    }
+	@HostListener('window:beforeunload')
+	private onReload() {
+		window.sessionStorage.setItem('scrollPosition', window.scrollY.toString());
+	}
 
-    scrollToPosition(value: number) {
-        setTimeout(() => {
-            window.scrollTo(0, value);
-            window.sessionStorage.removeItem('scrollPosition');
-        }, 50);
-    }
+	constructor(
+		private seo: SeoService,
+		private router: Router,
+		@Inject(DOCUMENT) private document: any,
+		private cookieConsentService: NgcCookieConsentService,
+		private readonly cookieService: CookieService
+	) {
+		const scrollItem = window.sessionStorage.getItem('scrollPosition');
+		this.scrollPosition = scrollItem || scrollItem == '0' ? Number(scrollItem) : 0;
+		this.$routerEventNavigationEnd = router.events.pipe(
+			filter(evt => evt instanceof NavigationEnd)
+		).subscribe(res => {
+			this.showFooterAndHeader = !!(this.router.url === notFoundPageUrl);
+		});
+	}
 
-    ngAfterViewInit() {
-        this.handleGoogleAnalyticsTracking();
-        setTimeout(() => {
-            if (this.scrollPosition) {
-                this.scrollToPosition(this.scrollPosition);
-            }
-        }, 300);
+	scrollToPosition(value: number) {
+		setTimeout(() => {
+			window.scrollTo(0, value);
+			window.sessionStorage.removeItem('scrollPosition');
+		}, 50);
+	}
 
-        this.$routeEvents = this.router.events
-            .pipe(filter((evt: any) => evt instanceof RoutesRecognized), pairwise())
-            .subscribe((events: RoutesRecognized[]) => {
-                const previousUrl = events[0].urlAfterRedirects;
-                const currentUrl = events[1].urlAfterRedirects;
-                if (previousUrl === currentUrl) {
-                    return;
-                }
+	ngOnInit() {
+		this.handleCookies();
+	}
 
-                const previousFragment = this.router.parseUrl(events[0].urlAfterRedirects).fragment;
-                const currentFragment = this.router.parseUrl(events[1].urlAfterRedirects).fragment;
-                const previousRouteUrl = this.router.parseUrl(events[0].urlAfterRedirects).root.children["primary"]?.segments[0].path;
-                const currentRouteUrl = this.router.parseUrl(events[1].urlAfterRedirects).root.children["primary"]?.segments[0].path;
-                if (previousUrl !== currentUrl && previousRouteUrl === currentRouteUrl) {
-                    if (previousFragment || currentFragment) {
-                        return;
-                    }
-                }
+	ngAfterViewInit() {
+		setTimeout(() => {
+			if (this.scrollPosition) {
+				this.scrollToPosition(this.scrollPosition);
+			}
+		}, 300);
 
-                this.scrollToPosition(0);
-            });
-    }
+		this.$routeEvents = this.router.events
+			.pipe(filter((evt: any) => evt instanceof RoutesRecognized), pairwise())
+			.subscribe((events: RoutesRecognized[]) => {
+				const previousUrl = events[0].urlAfterRedirects;
+				const currentUrl = events[1].urlAfterRedirects;
+				if (previousUrl === currentUrl) {
+					return;
+				}
 
-    ngOnDestroy() {
-        this.$routeEvents?.unsubscribe();
-        this.$routerEventNavigationEnd?.unsubscribe();
-        this.$gaRouteEvents?.unsubscribe();
-    }
+				const previousFragment = this.router.parseUrl(events[0].urlAfterRedirects).fragment;
+				const currentFragment = this.router.parseUrl(events[1].urlAfterRedirects).fragment;
+				const previousRouteUrl = this.router.parseUrl(events[0].urlAfterRedirects).root.children['primary']?.segments[0].path;
+				const currentRouteUrl = this.router.parseUrl(events[1].urlAfterRedirects).root.children['primary']?.segments[0].path;
+				if (previousUrl !== currentUrl && previousRouteUrl === currentRouteUrl) {
+					if (previousFragment || currentFragment) {
+						return;
+					}
+				}
 
-    private handleGoogleAnalyticsTracking(): void {
-        let configured = false;
-        this.$gaRouteEvents = this.router.events.subscribe((event) => {
-          if (event instanceof NavigationEnd) {
-            if(!configured){
-                gtag('config', 'UA-73071494-2', { send_page_view: false });
-                configured = true;
-            }
-            gtag('event', 'page_view', {
-                page_location: window.location.href,
-                page_path: event.urlAfterRedirects,
-            });
-          }
-        });
-      }
+				this.scrollToPosition(0);
+			});
+	}
+
+	ngOnDestroy() {
+		this.$routeEvents?.unsubscribe();
+		this.$routerEventNavigationEnd?.unsubscribe();
+		this.$gaRouteEvents?.unsubscribe();
+		this.statusChangeSubscription.unsubscribe();
+	}
+
+	private handleGoogleAnalyticsTracking(): void {
+		let configured = false;
+		this.$gaRouteEvents = this.router.events.subscribe((event) => {
+			if (event instanceof NavigationEnd) {
+				if (!configured) {
+					gtag('config', 'UA-73071494-2', { send_page_view: false });
+					configured = true;
+				}
+				gtag('event', 'page_view', {
+					page_location: window.location.href,
+					page_path: event.urlAfterRedirects,
+				});
+			}
+		});
+	}
+
+	private handleCookies(): void {
+		this.cookieService.get('cookieconsent_status')
+			? this.handleGoogleAnalyticsTracking()
+			: this.cookieService.deleteAll();
+
+		this.statusChangeSubscription.add(this.cookieConsentService.statusChange$
+			.subscribe(() => this.handleGoogleAnalyticsTracking())
+		);
+	}
 }
--- apps/valor-software-site/src/app/app.module.ts
+++ apps/valor-software-site/src/app/app.module.ts
@@ -3,33 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routing';
 import { AppComponent } from './app.component';
-import { CommonDocsModule, ARTICLES_LIST, SeoService, PORTFOLIO_LIST, ARTICLES_REFACTORED_TITLE_LIST, OLD_ROUTES_FROM_OLD_SITE } from '@valor-software/common-docs';
+import {
+	ARTICLES_LIST,
+	ARTICLES_REFACTORED_TITLE_LIST,
+	CommonDocsModule,
+	OLD_ROUTES_FROM_OLD_SITE,
+	PORTFOLIO_LIST,
+	SeoService
+} from '@valor-software/common-docs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ScullyLibModule } from '@scullyio/ng-lib';
-import { articlesList, articlesRefactoringTitlesList } from "../../../../assets/articles/articlesList";
-import { projectsList } from "../assets/portfolio/portfolio.list";
-import { linksFromOldSite } from "../../../../assets/articles/brokenRoutes";
+import { articlesList, articlesRefactoringTitlesList } from '../../../../assets/articles/articlesList';
+import { projectsList } from '../assets/portfolio/portfolio.list';
+import { linksFromOldSite } from '../../../../assets/articles/brokenRoutes';
 import { NotFoundComponent } from './404.component';
+import { NgcCookieConsentModule } from 'ngx-cookieconsent';
+import { cookieConfig } from '../configs';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NotFoundComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes, {anchorScrolling: 'enabled', onSameUrlNavigation: 'reload'}),
-    CommonDocsModule,
-    BrowserAnimationsModule,
-    ScullyLibModule
-  ],
-  providers: [
-    SeoService,
-    { provide: ARTICLES_LIST, useValue: articlesList },
-    { provide: ARTICLES_REFACTORED_TITLE_LIST, useValue: articlesRefactoringTitlesList },
-    { provide: PORTFOLIO_LIST, useValue: projectsList },
-    { provide: OLD_ROUTES_FROM_OLD_SITE, useValue: linksFromOldSite },
-  ],
-  bootstrap: [AppComponent],
+	declarations: [
+		AppComponent,
+		NotFoundComponent
+	],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot(routes, { anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' }),
+		CommonDocsModule,
+		BrowserAnimationsModule,
+		ScullyLibModule,
+		NgcCookieConsentModule.forRoot(cookieConfig)
+	],
+	providers: [
+		SeoService,
+		{ provide: ARTICLES_LIST, useValue: articlesList },
+		{ provide: ARTICLES_REFACTORED_TITLE_LIST, useValue: articlesRefactoringTitlesList },
+		{ provide: PORTFOLIO_LIST, useValue: projectsList },
+		{ provide: OLD_ROUTES_FROM_OLD_SITE, useValue: linksFromOldSite },
+	],
+	bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
