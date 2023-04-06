a254455f8eb89e975a30f26497cb8e8ba5df1e0b
Danyl Zhmyrov <33990613+ZhmenZH@users.noreply.github.com>
2023-04-06 12:26:05 +0300

    Feat: implemented cookie consent banner (#287)
    * fix: banner image and images on blog page (#277)
    
    * feat(ci): add automerge
    
    * feat(hotfix): the year on the footer updated
    
    * fix automerge
    
    * change github token
    
    * revert github token
    
    * Feat: implemented cookie consent banner
    
    * feat: removed unused code
    
    * feat: fixed comments
    
    * fix: triggering google analytics on the route after allowing cookies
    
    ---------
    
    Co-authored-by: Sergey Melnik <sergey.melnik@valor-software.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Oleksandr Yankevych <alexveselun@gmail.com>
    Co-authored-by: Sergei Melnik <sergei.melnik1980@gmail.com>

--- apps/valor-software-site/src/app/app.component.ts
+++ apps/valor-software-site/src/app/app.component.ts
@@ -1,101 +1,120 @@
-import { Component, Inject, HostListener, OnDestroy, AfterViewInit } from '@angular/core';
-import { SeoService } from "@valor-software/common-docs";
+import { AfterViewInit, Component, HostListener, OnDestroy } from '@angular/core';
+import { SeoService } from '@valor-software/common-docs';
 import { NavigationEnd, Router, RoutesRecognized } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
-import { filter, pairwise } from "rxjs/operators";
-import { Subscription } from "rxjs";
+import { filter, pairwise } from 'rxjs/operators';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 const notFoundPageUrl = '/404';
 declare const gtag: any;
+const isCookieAllowed = 'isCookieAllowed';
 
 @Component({
-  selector: 'valor-software-site-base-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'valor-software-site-base-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnDestroy, AfterViewInit {
-    title = 'valor-software-site';
-    scrollPosition = 0;
-    $routeEvents?: Subscription;
-    $gaRouteEvents?: Subscription;
-    $routerEventNavigationEnd: Subscription;
-    showFooterAndHeader = true;
+	title = 'valor-software-site';
+	scrollPosition = 0;
+	$routeEvents?: Subscription;
+	$gaRouteEvents?: Subscription;
+	$routerEventNavigationEnd: Subscription;
+	showFooterAndHeader = true;
+	showCookieConsentBanner = new BehaviorSubject(!this.cookieService.get(isCookieAllowed));
 
-    @HostListener('window:beforeunload') private onReload() {
-        window.sessionStorage.setItem('scrollPosition', window.scrollY.toString());
-    }
+	@HostListener('window:beforeunload')
+	private onReload() {
+		window.sessionStorage.setItem('scrollPosition', window.scrollY.toString());
+	}
 
-    constructor(
-      private seo: SeoService,
-      private router: Router,
-      @Inject(DOCUMENT) private document: any
-    ) {
-        const scrollItem = window.sessionStorage.getItem('scrollPosition');
-        this.scrollPosition = scrollItem || scrollItem == '0' ? Number(scrollItem) : 0;
-        this.$routerEventNavigationEnd = router.events.pipe(
-            filter(evt => evt instanceof NavigationEnd)
-        ).subscribe(res => {
-            this.showFooterAndHeader = !!(this.router.url === notFoundPageUrl);
-        });
-    }
+	constructor(
+		private seo: SeoService,
+		private router: Router,
+		private readonly cookieService: CookieService,
+	) {
+		const scrollItem = window.sessionStorage.getItem('scrollPosition');
+		this.scrollPosition = scrollItem || scrollItem == '0' ? Number(scrollItem) : 0;
+		this.$routerEventNavigationEnd = router.events.pipe(
+			filter(evt => evt instanceof NavigationEnd)
+		).subscribe(res => {
+			this.showFooterAndHeader = !!(this.router.url === notFoundPageUrl);
+		});
+	}
 
-    scrollToPosition(value: number) {
-        setTimeout(() => {
-            window.scrollTo(0, value);
-            window.sessionStorage.removeItem('scrollPosition');
-        }, 50);
-    }
+	ngAfterViewInit() {
+		this.cookieService.get(isCookieAllowed)
+			? this.handleGoogleAnalyticsTracking()
+			: this.cookieService.deleteAll();
 
-    ngAfterViewInit() {
-        this.handleGoogleAnalyticsTracking();
-        setTimeout(() => {
-            if (this.scrollPosition) {
-                this.scrollToPosition(this.scrollPosition);
-            }
-        }, 300);
+		setTimeout(() => {
+			if (this.scrollPosition) {
+				this.scrollToPosition(this.scrollPosition);
+			}
+		}, 300);
 
-        this.$routeEvents = this.router.events
-            .pipe(filter((evt: any) => evt instanceof RoutesRecognized), pairwise())
-            .subscribe((events: RoutesRecognized[]) => {
-                const previousUrl = events[0].urlAfterRedirects;
-                const currentUrl = events[1].urlAfterRedirects;
-                if (previousUrl === currentUrl) {
-                    return;
-                }
+		this.$routeEvents = this.router.events
+			.pipe(filter((evt: any) => evt instanceof RoutesRecognized), pairwise())
+			.subscribe((events: RoutesRecognized[]) => {
+				const previousUrl = events[0].urlAfterRedirects;
+				const currentUrl = events[1].urlAfterRedirects;
+				if (previousUrl === currentUrl) {
+					return;
+				}
 
-                const previousFragment = this.router.parseUrl(events[0].urlAfterRedirects).fragment;
-                const currentFragment = this.router.parseUrl(events[1].urlAfterRedirects).fragment;
-                const previousRouteUrl = this.router.parseUrl(events[0].urlAfterRedirects).root.children["primary"]?.segments[0].path;
-                const currentRouteUrl = this.router.parseUrl(events[1].urlAfterRedirects).root.children["primary"]?.segments[0].path;
-                if (previousUrl !== currentUrl && previousRouteUrl === currentRouteUrl) {
-                    if (previousFragment || currentFragment) {
-                        return;
-                    }
-                }
+				const previousFragment = this.router.parseUrl(events[0].urlAfterRedirects).fragment;
+				const currentFragment = this.router.parseUrl(events[1].urlAfterRedirects).fragment;
+				const previousRouteUrl = this.router.parseUrl(events[0].urlAfterRedirects).root.children['primary']?.segments[0].path;
+				const currentRouteUrl = this.router.parseUrl(events[1].urlAfterRedirects).root.children['primary']?.segments[0].path;
+				if (previousUrl !== currentUrl && previousRouteUrl === currentRouteUrl) {
+					if (previousFragment || currentFragment) {
+						return;
+					}
+				}
 
-                this.scrollToPosition(0);
-            });
-    }
+				this.scrollToPosition(0);
+			});
+	}
 
-    ngOnDestroy() {
-        this.$routeEvents?.unsubscribe();
-        this.$routerEventNavigationEnd?.unsubscribe();
-        this.$gaRouteEvents?.unsubscribe();
-    }
+	ngOnDestroy() {
+		this.$routeEvents?.unsubscribe();
+		this.$routerEventNavigationEnd?.unsubscribe();
+		this.$gaRouteEvents?.unsubscribe();
+	}
 
-    private handleGoogleAnalyticsTracking(): void {
-        let configured = false;
-        this.$gaRouteEvents = this.router.events.subscribe((event) => {
-          if (event instanceof NavigationEnd) {
-            if(!configured){
-                gtag('config', 'UA-73071494-2', { send_page_view: false });
-                configured = true;
-            }
-            gtag('event', 'page_view', {
-                page_location: window.location.href,
-                page_path: event.urlAfterRedirects,
-            });
-          }
-        });
-      }
+	onAcceptCookies(): void {
+		this.cookieService.set(isCookieAllowed, 'true');
+		this.showCookieConsentBanner.next(false);
+		/** make navigation to current url to trigger googleAnalytics on current route */
+		this.redirectTo(this.router.url);
+		this.handleGoogleAnalyticsTracking();
+	}
+
+	private scrollToPosition(value: number): void {
+		setTimeout(() => {
+			window.scrollTo(0, value);
+			window.sessionStorage.removeItem('scrollPosition');
+		}, 50);
+	}
+
+	private handleGoogleAnalyticsTracking(): void {
+		let configured = false;
+		this.$gaRouteEvents = this.router.events.subscribe((event) => {
+			if (event instanceof NavigationEnd) {
+				if (!configured) {
+					gtag('config', 'UA-73071494-2', { send_page_view: false });
+					configured = true;
+				}
+				gtag('event', 'page_view', {
+					page_location: window.location.href,
+					page_path: event.urlAfterRedirects,
+				});
+			}
+		});
+	}
+
+	private redirectTo(uri: string): void {
+		this.router.navigateByUrl('/', { skipLocationChange: true })
+			.then(() => this.router.navigate([uri]));
+	}
 }
