31dae24a9d77c546a445f2ad5b674e75234a71cd
danyaZh <reus11fan@gmail.com>
2023-07-19 16:07:51 +0300

    feat: redesign of footer

--- libs/common-docs/src/components/app-footer/app-footer.component.ts
+++ libs/common-docs/src/components/app-footer/app-footer.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
-import { ModalService } from "../../services/modal.service";
-import { ContactModalComponent } from "../contact-modal/contactModal.components";
+import { ModalService } from '../../services/modal.service';
+import { ContactModalComponent } from '../contact-modal/contactModal.components';
 
 @Component({
-    // eslint-disable-next-line @angular-eslint/component-selector
-    selector: 'app-footer',
-    templateUrl: './app-footer.component.html'
+	// eslint-disable-next-line @angular-eslint/component-selector
+	selector: 'app-footer',
+	templateUrl: './app-footer.component.html',
+	styleUrls: ['./app-footer.component.scss']
 })
 export class AppFooterComponent {
-    constructor(
-        private modalService: ModalService<ContactModalComponent>
-    ) {
-    }
+	constructor(
+		private modalService: ModalService<ContactModalComponent>
+	) {
+	}
 
-    openModal() {
-        this.modalService.open(ContactModalComponent);
-    }
+	openModal() {
+		this.modalService.open(ContactModalComponent);
+	}
 }
