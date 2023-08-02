40a11a1f022d34ab842d74fd7b8c5d61312a73a3
danyaZh <reus11fan@gmail.com>
2023-08-02 18:25:21 +0300

    feat: redesign of footer

--- libs/common-docs/src/components/app-footer/app-footer.component.ts
+++ libs/common-docs/src/components/app-footer/app-footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 import { ContactModalComponent } from '../contact-modal/contactModal.components';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
 	// eslint-disable-next-line @angular-eslint/component-selector
@@ -9,12 +10,25 @@ import { ContactModalComponent } from '../contact-modal/contactModal.components'
 	styleUrls: ['./app-footer.component.scss']
 })
 export class AppFooterComponent {
+	emailControl = new FormControl<string>(
+		'', [Validators.required, Validators.email]
+	);
+	readonly validationMessage = {
+		type: 'email',
+		message: 'Please enter a valid email'
+	};
+
 	constructor(
 		private modalService: ModalService<ContactModalComponent>
 	) {
 	}
 
-	openModal() {
+	openModal(): void {
 		this.modalService.open(ContactModalComponent);
+	}
+
+	hasEmailError(): boolean {
+		return this.emailControl.hasError(this.validationMessage.type)
+			&& (this.emailControl.dirty || this.emailControl.touched);
 	}
 }
