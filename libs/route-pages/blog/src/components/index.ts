1b251c1d68dea1ebb0caa2320b6f10d07b4aca2d
danyaZh <reus11fan@gmail.com>
2023-10-31 15:10:41 +0200

    feat: created new design for blog page

--- libs/route-pages/blog/src/components/index.ts
+++ libs/route-pages/blog/src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './article/article.component';
 export * from './blog/blog.component';
+export * from './blog-item/blog-item.component';
---
+++ libs/route-pages/blog/src/models/blog.consts.ts
@@ -0,0 +1,15 @@
+export const Domains: Record<string, string> = {
+	business_analysis: 'Business Analysis',
+	user_research_design: 'User Research & Design',
+	dev_quality_assurance: 'Development & Quality Assurance',
+	devops_cloud: 'DevOps & Cloud',
+	recruitment_pm: 'Recruitment & Project Mgmt.',
+	sales_marketing: 'Sales & Marketing',
+	module_federation: 'Module Federation',
+	other: 'Other'
+};
+
+export const Languages = {
+	en: 'English',
+	pt: 'Portuguese'
+};
